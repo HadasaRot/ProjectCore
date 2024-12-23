9e17eab3e753f963e3f119faf103414e6290763d
HadasaRot <h4104901@gmail.com>
2024-12-23 16:47:42 +0200

    Initial commit

---
+++ models/Book.cs
@@ -0,0 +1,16 @@
+namespace ProjectCore;
+public enum CategoryBooks
+{
+    Biography,    
+    Textbook,   
+    Tension,   
+    History, 
+    Science,    
+    Philosophy  
+}
+public class Book
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public CategoryBooks Category { get; set; }
+}
