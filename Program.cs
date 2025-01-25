7313c58770d71c5e99066d10cc05d7632ab7408e
HadasaRot <h4104901@gmail.com>
2025-01-26 01:25:02 +0200

    Adding writing to the log

--- Program.cs
+++ Program.cs
@@ -5,9 +5,20 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using ProjectCore.Middlewares;
+using Serilog;
+using Serilog.Events;
 
 
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.DateFormatPath(
+        pathFormat: "Logs/log-{date:format=yyyy-MM-dd}.txt"
+    )
+    .CreateLogger();
+builder.Host.UseSerilog();
+
 
 builder.Services.AddControllers();
 
@@ -23,6 +34,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -30,9 +43,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseLoggerMiddleware();
 app.UseErrorHandlingMiddleware();
-
 /*js*/
 app.UseDefaultFiles();
 app.UseStaticFiles();
@@ -44,3 +56,6 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+
